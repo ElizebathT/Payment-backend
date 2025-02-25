7d671e5816589d2ea66f0c2d4116eb26746bab43
Elizebath <elizebaththomasv@gmail.com>
2025-02-25 11:28:06 +0530

    user ok

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -3,8 +3,8 @@ const stripeController = require("../controllers/stripeController");
 const userAuthentication = require("../middlewares/userAuthentication");
 const stripeRouter = express.Router();
 
-stripeRouter.post('/verify', userAuthentication,express.raw({ type: 'application/json' }), stripeController.verify);
+stripeRouter.post('/verify', express.raw({ type: 'application/json' }), stripeController.verify);
 express.json()
-stripeRouter.post("/checkout", userAuthentication,stripeController.payment);
+stripeRouter.post("/checkout", stripeController.payment);
 
 module.exports = stripeRouter;
