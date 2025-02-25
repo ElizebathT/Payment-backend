e36f29fc965e6e9f0730fd5ac058b9e33defa4c6
Elizebath <elizebaththomasv@gmail.com>
2025-02-25 09:19:18 +0530

    post method

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -5,7 +5,7 @@ const stripeRouter = express.Router();
 
 
 // stripeRouter.get('/verify/:paymentId', stripeController.verify);
-stripeRouter.get('/verify', express.raw({ type: 'application/json' }), stripeController.verify);
+stripeRouter.post('/verify', express.raw({ type: 'application/json' }), stripeController.verify);
 express.json()
 stripeRouter.post("/checkout", stripeController.payment);
 
